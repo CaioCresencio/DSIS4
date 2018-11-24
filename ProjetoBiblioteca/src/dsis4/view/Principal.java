52ac7073c498eb0c09289446ac576755ba33748c
GabrielBergamim <gabrielbergamim@users.noreply.github.com>
2018-11-24 18:06:43 -0200

    Update Principal.java

--- ProjetoBiblioteca/src/dsis4/view/Principal.java
+++ ProjetoBiblioteca/src/dsis4/view/Principal.java
@@ -1,10 +1,8 @@
 package dsis4.view;
 
-import dsis4.dao.ObraDAO;
-import dsis4.entidades.ObraLiteraria;
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
+import dsis4.entidades.CategoriaObra;
+import dsis4.entidades.ListaObra;
+import dsis4.xml.LeitorJAXB;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -19,12 +17,28 @@ import java.util.List;
 public class Principal {
     public static void main(String[] args) {
         
-        ObraDAO obra = new ObraDAO();
+        /*ObraDAO obra = new ObraDAO();
         List<String> autores = new ArrayList();
         autores.add("euton");
         ObraLiteraria o = new ObraLiteraria("23311", 2, 2, LocalDate.now(), "Castanha", "JAsvaldo", 2, autores);
-        obra.salvar(o);
+        obra.salvar(o);*/
         //JanelaPrincipal janelaPrincipal = new JanelaPrincipal();
+        
+        /*LeitorSAX s = new LeitorSAX("obra.xml");
+        
+        ListaObra lista = (ListaObra) s.ler();
+        
+        System.out.println(lista);*/
+        
+        LeitorJAXB s = new LeitorJAXB("obra.xml");
+        
+        s.setClasses(ListaObra.class);
+        
+        ListaObra lista = (ListaObra) s.ler();
+        
+        System.out.println(lista);
+        
+        
     }
     
 }
