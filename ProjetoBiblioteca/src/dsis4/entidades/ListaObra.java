069502744d11a9c8706a9137db8613c911d5e956
GabrielBergamim <gabrielbergamim@users.noreply.github.com>
2018-11-24 18:06:37 -0200

    Create ListaObra.java

---
+++ ProjetoBiblioteca/src/dsis4/entidades/ListaObra.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dsis4.entidades;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+
+@XmlRootElement(name = "obras")
+public class ListaObra {
+    @XmlElement(name = "obra")
+    private List<ObraLiteraria> obras;
+    
+    public ListaObra(List<ObraLiteraria> obras){
+        this.obras = obras;
+    }
+    
+    public ListaObra(){};
+    
+    public void addObra(ObraLiteraria obra){
+        obras.add(obra);
+    }
+    
+    @Override
+    public String toString() {
+        return obras.toString();
+    }
+}
