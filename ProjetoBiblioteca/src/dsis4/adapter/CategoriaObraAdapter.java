bb9b9509af1c31c17677d33144fe1c82ac9b542e
GabrielBergamim <gabrielbergamim@users.noreply.github.com>
2018-11-28 22:25:40 -0200

    Update CategoriaObraAdapter.java

--- ProjetoBiblioteca/src/dsis4/adapter/CategoriaObraAdapter.java
+++ ProjetoBiblioteca/src/dsis4/adapter/CategoriaObraAdapter.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package dsis4adapter;
+package dsis4.adapter;
 
 import dsis4.entidades.CategoriaObra;
 import javax.xml.bind.annotation.adapters.XmlAdapter;
