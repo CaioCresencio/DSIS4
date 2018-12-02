a6257b6612cf50a05065fd9fe2d8339958e97cb0
GabrielBergamim <gabrielbergamim@users.noreply.github.com>
2018-12-02 10:36:15 -0200

    Update GravadorPDF.java

--- ProjetoBiblioteca/src/dsis4/relatorioPDF/GravadorPDF.java
+++ ProjetoBiblioteca/src/dsis4/relatorioPDF/GravadorPDF.java
@@ -8,43 +8,84 @@ package dsis4.relatorioPDF;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.Phrase;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
 import dsis4.entidades.ListaObra;
+import dsis4.entidades.ObraLiteraria;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class GravadorPDF {
-    String arquivo;
+    private String arquivo;
+    private PdfPTable tabela;
     
     public GravadorPDF(String arquivo){
         this.arquivo = arquivo;
     }
    
     public void criaPDF(ListaObra obras) {
-        
-        
+            
         try{
             Document document = new Document();
             PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream(arquivo));
             
             document.open();
             
-            Paragraph paragraph = new Paragraph("Obras Literarias");
+            Paragraph paragraph = new Paragraph("RELATÓRIO\n\n");
+            document.add(paragraph);
+            paragraph = new Paragraph("Obras Literárias:\n\n");
+            document.add(paragraph);
             
+            tabela = new PdfPTable(6);
+            headerTabela();
+ 
+            preencheTabela(obras);
             
+            document.add(tabela);
             
-            
-            
-            
+            document.close();
+        
         } catch (FileNotFoundException ex) {
             Logger.getLogger(GravadorPDF.class.getName()).log(Level.SEVERE, null, ex);
         } catch (DocumentException ex) {
             Logger.getLogger(GravadorPDF.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+               
+    }
+    
+    
+    private void headerTabela(){
+            PdfPCell c = new PdfPCell(new Phrase("Título"));
+            tabela.addCell(c);
+            c = new PdfPCell(new Phrase("ISBN"));
+            tabela.addCell(c);
+            c = new PdfPCell(new Phrase("Edição"));
+            tabela.addCell(c);
+            c = new PdfPCell(new Phrase("Categoria"));
+            tabela.addCell(c);
+            c = new PdfPCell(new Phrase("Acervo"));
+            tabela.addCell(c);
+            /*c = new PdfPCell(new Phrase("Disponiveis"));
+            tabela.addCell(c);*/
+            c = new PdfPCell(new Phrase("Autor(es)"));
+            tabela.addCell(c);            
+            tabela.setHeaderRows(1);
+    }
+    
+    private void preencheTabela(ListaObra obras){
+        for(ObraLiteraria o: obras.getLista()){                
+            tabela.addCell(o.getTitulo());
+            tabela.addCell(o.getIsbn());
+            tabela.addCell(String.valueOf(o.getNrmEdicao()));
+            tabela.addCell(o.getCategoria().getDescricao());
+            tabela.addCell(String.valueOf(o.getQtdExemplares()));
+            //tabela.addCell(String.valueOf(o.getQtdDisponivel()));
+            tabela.addCell(String.valueOf(o.getAutores().toString()));             
+        }
         
     }
 }
