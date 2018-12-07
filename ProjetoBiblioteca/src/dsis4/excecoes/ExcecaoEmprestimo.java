90249882a55e0dd960b23a78708b366945580147
GabrielBergamim <gabrielbergamim@users.noreply.github.com>
2018-12-06 22:45:04 -0200

    Create ExcecaoEmprestimo.java

---
+++ ProjetoBiblioteca/src/dsis4/excecoes/ExcecaoEmprestimo.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dsis4.excecoes;
+
+/**
+ *
+ * @author Windows
+ */
+public class ExcecaoEmprestimo extends Exception{
+    public ExcecaoEmprestimo(){
+        super("Erro ao realizar emprestimo");
+    }
+    
+}
